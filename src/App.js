f60eb52684685f737be0b1c1b0a032c09bc50de2
Saul Zapata <85974941+iamSaulZapata@users.noreply.github.com>
2023-04-30 19:40:15 +0000

    Added CRUD layout HTML

--- src/App.js
+++ src/App.js
@@ -1,29 +1,84 @@
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
-  );
+	const [users, setUsers] = useState([
+		{ id: 1, name: "John", email: "john@example.com" },
+		{ id: 2, name: "Jane", email: "jane@example.com" },
+		{ id: 3, name: "Bob", email: "bob@example.com" },
+	]);
+	const [selectedUser, setSelectedUser] = useState(null);
+	const [name, setName] = useState("");
+	const [email, setEmail] = useState("");
+
+	const handleAddUser = () => {
+		const newUser = { id: Date.now(), name, email };
+		setUsers([...users, newUser]);
+		setName("");
+		setEmail("");
+	};
+
+	const handleUpdateUser = () => {
+		const updatedUser = { ...selectedUser, name, email };
+		const updatedUsers = users.map((user) =>
+			user.id === selectedUser.id ? updatedUser : user
+		);
+		setUsers(updatedUsers);
+		setSelectedUser(null);
+		setName("");
+		setEmail("");
+	};
+
+	const handleDeleteUser = (id) => {
+		const filteredUsers = users.filter((user) => user.id !== id);
+		setUsers(filteredUsers);
+	};
+
+	const handleEditUser = (id) => {
+		const user = users.find((user) => user.id === id);
+		setSelectedUser(user);
+		setName(user.name);
+		setEmail(user.email);
+	};
+
+	return (
+		<div>
+			<h1>Users</h1>
+			<ul>
+				{users.map((user) => (
+					<li key={user.id}>
+						{user.name} ({user.email}){" "}
+						<button onClick={() => handleEditUser(user.id)}>Edit</button>{" "}
+						<button onClick={() => handleDeleteUser(user.id)}>Delete</button>
+					</li>
+				))}
+			</ul>
+			<h2>{selectedUser ? "Edit User" : "Add User"}</h2>
+			<div>
+				<label>Name:</label>
+				<input
+					type="text"
+					value={name}
+					onChange={(e) => setName(e.target.value)}
+				/>
+			</div>
+			<div>
+				<label>Email:</label>
+				<input
+					type="text"
+					value={email}
+					onChange={(e) => setEmail(e.target.value)}
+				/>
+			</div>
+			<div>
+				{selectedUser ? (
+					<button onClick={handleUpdateUser}>Update User</button>
+				) : (
+					<button onClick={handleAddUser}>Add User</button>
+				)}
+			</div>
+		</div>
+	);
 }
 
 export default App;
