9f7e1572882e82015266ce2d4ffa6ce665dde587
Saul Zapata <85974941+iamSaulZapata@users.noreply.github.com>
2023-04-30 21:04:03 +0000

    Update to layout in HTML

--- src/App.js
+++ src/App.js
@@ -1,30 +1,24 @@
-import "./App.css";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function App() {
-	const [users, setUsers] = useState([
-		{ id: 1, name: "John", email: "john@example.com" },
-		{ id: 2, name: "Jane", email: "jane@example.com" },
-		{ id: 3, name: "Bob", email: "bob@example.com" },
-	]);
-	const [selectedUser, setSelectedUser] = useState(null);
+	const [users, setUsers] = useState([]);
 	const [name, setName] = useState("");
 	const [email, setEmail] = useState("");
+
+	useEffect(() => {
+		const storedUsers = localStorage.getItem("users");
+		if (storedUsers) {
+			setUsers(JSON.parse(storedUsers));
+		}
+	}, []);
+
+	useEffect(() => {
+		localStorage.setItem("users", JSON.stringify(users));
+	}, [users]);
 
 	const handleAddUser = () => {
 		const newUser = { id: Date.now(), name, email };
 		setUsers([...users, newUser]);
-		setName("");
-		setEmail("");
-	};
-
-	const handleUpdateUser = () => {
-		const updatedUser = { ...selectedUser, name, email };
-		const updatedUsers = users.map((user) =>
-			user.id === selectedUser.id ? updatedUser : user
-		);
-		setUsers(updatedUsers);
-		setSelectedUser(null);
 		setName("");
 		setEmail("");
 	};
@@ -34,13 +28,6 @@ function App() {
 		setUsers(filteredUsers);
 	};
 
-	const handleEditUser = (id) => {
-		const user = users.find((user) => user.id === id);
-		setSelectedUser(user);
-		setName(user.name);
-		setEmail(user.email);
-	};
-
 	return (
 		<div>
 			<h1>Users</h1>
@@ -48,12 +35,11 @@ function App() {
 				{users.map((user) => (
 					<li key={user.id}>
 						{user.name} ({user.email}){" "}
-						<button onClick={() => handleEditUser(user.id)}>Edit</button>{" "}
 						<button onClick={() => handleDeleteUser(user.id)}>Delete</button>
 					</li>
 				))}
 			</ul>
-			<h2>{selectedUser ? "Edit User" : "Add User"}</h2>
+			<h2>Add User</h2>
 			<div>
 				<label>Name:</label>
 				<input
@@ -71,11 +57,7 @@ function App() {
 				/>
 			</div>
 			<div>
-				{selectedUser ? (
-					<button onClick={handleUpdateUser}>Update User</button>
-				) : (
-					<button onClick={handleAddUser}>Add User</button>
-				)}
+				<button onClick={handleAddUser}>Add User</button>
 			</div>
 		</div>
 	);
